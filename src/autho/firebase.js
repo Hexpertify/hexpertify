f36e4d9ff9395e0391fa7d4306e97c4602ff26d8
Hexpertify <hexpertifyapp@gmail.com>
2024-08-05 14:15:52 +0530

    Update firebase.js

--- src/autho/firebase.js
+++ src/autho/firebase.js
@@ -10,11 +10,12 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyDtfmJ1-d3RA9rcdGjYrZT-4xlthO0Z5YM",
   authDomain: "consultantapp-1b838.firebaseapp.com",
+  databaseURL: "https://consultantapp-1b838-default-rtdb.firebaseio.com",
   projectId: "consultantapp-1b838",
   storageBucket: "consultantapp-1b838.appspot.com",
   messagingSenderId: "238366414583",
-  appId: "1:238366414583:web:81be893c7e7b1e004325f2",
-  measurementId: "G-TN2NN6N1PM"
+  appId: "1:238366414583:web:377cdf2e354bdb624325f2",
+  measurementId: "G-JMM3WD6ZSG"
 };
 
 // Initialize Firebase
